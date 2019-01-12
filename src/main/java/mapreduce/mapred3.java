84b0475a0bcaf9424333082e493b6aa6811decee
Xairon <ringuetnicolas2@gmail.com>
2019-01-12 17:49:18 +0100

    mapred4

--- src/main/java/mapreduce/mapred3.java
+++ src/main/java/mapreduce/mapred3.java
@@ -59,7 +59,7 @@ public class mapred3 {
 
                 byte[] nom = result.getValue(Bytes.toBytes("#"), Bytes.toBytes("N"));
                 name = Bytes.toString(nom);
-                key = name+"/"+oKey2+"/"+inKey;
+                key = name+"/"+oKey2+"/"+oKey;
 
             }
             finally {
